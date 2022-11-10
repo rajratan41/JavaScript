1fdf4cf560d2d8a974d56d1a80fb5b9c9e36fc07
Raj Ratan <raj808341@gmail.com>
2022-11-10 22:09:45 +0530

    adding Date and Time

---
+++ 06DateAndTime/DateAndTime.js
@@ -0,0 +1,16 @@
+let now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+
+
+console.log(now.toString());
+
+console.log(now);
+
+
+
