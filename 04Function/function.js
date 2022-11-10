bc67fe680196b36470816bafd53c910a0c286a05
Raj Ratan <raj808341@gmail.com>
2022-11-10 21:32:54 +0530

    adding function

---
+++ 04Function/function.js
@@ -0,0 +1,60 @@
+// Function - It make something reusable with same logic of code
+//          - we can write clean & redeable code
+
+// Create a Program using Function for finding Square
+
+// function without parameter
+
+// function Square () {
+//     let x = 5;
+//     let sq = x * x;
+//     console.log(sq);
+// }
+// Square();
+
+// function with parameter
+
+// function add (x,y) {
+//     let xy = x+y;
+//     console.log(xy);
+// }
+// add(5,2);
+
+
+// RETURN
+
+// function printFullName(firstName, lastName) {
+//     let fullName = firstName + " "  +lastName;
+//     return fullName;
+// }
+
+// let fn = printFullName("Raj", "Ratan");
+// console.log(fn);
+
+
+// Create a program for calculating Area of Circle
+
+// function areaOfCircle (r) {
+//     let area = Math.PI * r*r;
+//     return area;
+// }
+
+// let ar = areaOfCircle(25);
+// console.log(ar);
+
+
+// Create a Program using Function for sum of numbers
+
+// function sumOfArray (arr) {
+//     let sum = 0;
+//     for (let i=0; i<arr.length; i++) {
+//         sum = sum + arr [i];
+//     }
+//     return sum;
+// }
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let holdValue = sumOfArray(num);
+
+// console.log(holdValue);
