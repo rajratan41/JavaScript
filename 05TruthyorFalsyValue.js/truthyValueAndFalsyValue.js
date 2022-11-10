bb411cf7f6196f4d36fd489015886c655072d04c
Raj Ratan <raj808341@gmail.com>
2022-11-10 21:55:05 +0530

    adding Truthy and Falsy Value

---
+++ 05TruthyorFalsyValue.js/truthyValueAndFalsyValue.js
@@ -0,0 +1,12 @@
+// TRUTHY VALUE -
+// - All Strings execpt Empty String
+// - All Positive and Negative numbers except 0
+// - Boolean= True
+
+
+// FALSY VALUE -
+//  - empty string
+//  - 0
+//  - null
+//  - Bollean= False
+// - All remaining are Falsy Value
