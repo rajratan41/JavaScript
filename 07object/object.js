8c747abf1a9e35771fb86545e8ee7860d66fff9d
Raj Ratan <raj808341@gmail.com>
2022-11-27 18:10:49 +0530

    adding object file

---
+++ 07object/object.js
@@ -0,0 +1,21 @@
+//  let userDetails = {
+//     firstName: "Raj",
+//     lastName: "Ratan",
+//     role: "admin",
+//     loginCount: 25,
+//  };
+
+//  console.log(userDetails);
+//  console.log(userDetails.firstName);
+//  console.log(userDetails.lastName);
+//  console.table(userDetails);
+
+// update value
+
+// userDetails.loginCount = 32;
+// console.log(userDetails);
+
+// Mini Program 
+// Method in object
+
+let 
