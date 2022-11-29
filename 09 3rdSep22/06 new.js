2088f5a4fbfdbf4fd5d79a8881e992aa0199a6f5
Raj Ratan <raj808341@gmail.com>
2022-11-29 21:32:18 +0530

    adding Set

--- 09 3rdSep22/06 new.js
+++ 09 3rdSep22/06 new.js
@@ -16,13 +16,39 @@
 
 // 2
 
-var person = function (firstName, courseCount) {
-    this.firstName = firstName;
-    this.courseCount = courseCount;
-};
+// var person = function (firstName, courseCount) {
+//     this.firstName = firstName;
+//     this.courseCount = courseCount;
+// };
 
-var aman = new person ("Aman", 4);
-console.log(aman);
+// var aman = new person ("Aman", 4);
+// console.log(aman);
 
-var raj = new person ("Raj", 6);
-console.log(raj);
+// var raj = new person ("Raj", 6);
+// console.log(raj);
+
+
+
+
+// Set - Collection of element which store unique element
+// - Set only contain Unique elements
+
+let a = [1, 2, 3, 1, 3, 2, 4, 3, 5, 3, 6, 3, 4, 1]
+
+let num = new Set (a);
+console.log(num);
+
+num.add(8);
+console.log(num);
+
+num.delete(3);
+console.log(num);
+
+
+console.log(num.has(5));
+console.log(num.has(9));
+
+// for (let i of num) {
+//     console.log(i);
+// };
+
