50d1d27ff9b5b6cd787b9c5f859ae34cff77eb92
Raj Ratan <raj808341@gmail.com>
2022-11-29 19:52:58 +0530

    adding destructuring

---
+++ 09 3rdSep22/03destructuring.js
@@ -0,0 +1,13 @@
+// Destructuring - When you have values inside an Array and You want to now pick out that value from Array and stored that value in a Variable
+
+// examples
+//1
+
+let sciValue = [2.72, 3.14, 9.81, 37, 100];
+
+let [e, pi, gravity, bodytemp, boil] = sciValue;
+
+console.log(e);
+console.log(gravity);
+console.log(boil);
+console.log(bodytemp);
