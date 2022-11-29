fdc6ebe4cdf0b5f5371b3a26ae1da44fb514d2fa
Raj Ratan <raj808341@gmail.com>
2022-11-29 20:41:16 +0530

    adding rest

--- 09 3rdSep22/05 rest.js
+++ 09 3rdSep22/05 rest.js
@@ -1,2 +1,13 @@
 // Rest operator
 
+function sumTwo (...args) {
+    console.log(args);
+
+    let sum = 0;
+    for (let a of args) {
+        sum = sum + a;
+    }
+    return sum;
+}
+
+console.log(sumTwo(1, 2, 3, 4, 5));
