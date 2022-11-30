796a5cad0d14921d16fe31ba03eadc2b85cb54cb
Raj Ratan <raj808341@gmail.com>
2022-11-30 18:24:57 +0530

    adding Map

--- 09 3rdSep22/06 new.js
+++ 09 3rdSep22/06 new.js
@@ -33,22 +33,50 @@
 // Set - Collection of element which store unique element
 // - Set only contain Unique elements
 
-let a = [1, 2, 3, 1, 3, 2, 4, 3, 5, 3, 6, 3, 4, 1]
+// let a = [1, 2, 3, 1, 3, 2, 4, 3, 5, 3, 6, 3, 4, 1]
 
-let num = new Set (a);
-console.log(num);
+// let num = new Set (a);
+// console.log(num);
 
-num.add(8);
-console.log(num);
+// num.add(8);
+// console.log(num);
 
-num.delete(3);
-console.log(num);
+// num.delete(3);
+// console.log(num);
 
 
-console.log(num.has(5));
-console.log(num.has(9));
+// console.log(num.has(5));
+// console.log(num.has(9));
 
 // for (let i of num) {
 //     console.log(i);
 // };
 
+
+// Maps - it holds key value pair , key can be of any data type (String, Number, Boolean, Array)
+
+// let newMap = new Map();
+// console.log(newMap);
+
+// setter - (set) = To set the Value
+
+// newMap.set("1", "One String");
+// newMap.set(1, "One Number");
+// newMap.set(true, "Boolean");
+
+// console.log(newMap);
+
+// Differrence b/w Object and Map -  => (Fat Arrow)
+
+// Object = {1: "One"}
+// Set = {1, 2, 3, 4, 5}
+// Map = { '1' => 'One String', 1 => 'One Number', true => 'Boolean' }
+
+
+let city = [ ["India", "Delhi"],["Gujrat", "Surat"],["Rajasthan", "Jaipur"] ];
+
+let newmap = new Map(city);
+console.log(newmap);
+
+// Getter - (get) = to get the value
+console.log(newmap.get("India"));
