aa0fe78bc4df5865b7a332e9762cfffa90632c04
Raj Ratan <raj808341@gmail.com>
2022-12-02 20:56:04 +0530

    adding scope

---
+++ 10 4Sep22/02 scope.js
@@ -0,0 +1,23 @@
+// Scope - {
+    
+        // Anything written inside {} is called as Scope
+
+//          }
+
+// Scope - Where to look for things => (all the variable we are assigning)
+//  Q. What is that we are looking for ?
+
+// example-     
+//   if () {
+//     let a = 23;
+//     let b = 42;
+//     // console.log(a + b); => console.log is looking for a and b i.e inside the scope
+//   }
+
+
+// JavaScript is Synchronus Single threaded language
+
+// Synchronus means One by One.
+// Single threaded means One line at a time.
+
+// Inside JavaScript everything happens inside Exexution Context
