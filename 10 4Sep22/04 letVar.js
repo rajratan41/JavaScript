2add3f5b7e474adbfe3183156191a5cb88981079
Raj Ratan <raj808341@gmail.com>
2022-12-03 19:09:01 +0530

    update let var

--- 10 4Sep22/04 letVar.js
+++ 10 4Sep22/04 letVar.js
@@ -1,8 +1,24 @@
 // Difference b/w Let and Var
 
 
-console.log(b);
-console.log(a);
-var a = 10;
-let b = 20;
+// console.log(b);
+// console.log(a);
+// var a = 10;
+// let b = 20;
 
+// Var - var is stored in Global
+
+// Let - let is stored in a different scope inside Global
+// Let - let cannot access before initialization because Let is in differernt scope inside Global
+//  Example
+
+// {   This is Global
+//      
+//      var a = 10;
+// 
+//      {   This is inside Global
+// 
+//          let b = 20; 
+// 
+//      }
+// }
