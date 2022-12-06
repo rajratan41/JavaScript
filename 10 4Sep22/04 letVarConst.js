a65429c191432bd0024cfc84b5e35358d7769847
Raj Ratan <raj808341@gmail.com>
2022-12-06 20:19:23 +0530

    adding let var const

--- 10 4Sep22/04 letVarConst.js
+++ 10 4Sep22/04 letVarConst.js
@@ -1,8 +1,8 @@
-// Difference b/w Let and Var
+// Difference b/w Let, Var, Const
 
 
-// console.log(b);
-// console.log(a);
+// console.log(b);  // gives error bcz we access let before initialization
+// console.log(a);  // gives undefine bcz var is stored in Global so it scanned and got undefined
 // var a = 10;
 // let b = 20;
 
@@ -24,16 +24,34 @@
 // }
 
 
-//  Example
+//  Example of Let
+
+// 1
 
 // console.log(a);   // access
 // let a = 10;  // Initialization
 
 // it will give error a can't be access before initialization
 
+// 2
+
 // let a = 10;  // Initialize
 // console.log(a);  // access
 
 // result = 10
 
-// First Initialize then acces here we did
+// First Initialize then access
+
+// Const - As soon as something is declared as Const it is mandatory to initialize value on the spot
+
+// example - 
+
+// 1
+//  const a;
+//  a = 10;
+//  this will give error bcz when a is declared as const then it is mandatory to put value
+
+
+// 2
+// const a = 10;    // Initialize
+// console.log(a);  // access
