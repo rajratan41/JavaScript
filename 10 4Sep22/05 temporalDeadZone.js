a3cfedc86fa6a3b21f448702a73fcdb6f81ab5e8
Raj Ratan <raj808341@gmail.com>
2022-12-06 20:36:36 +0530

    adding temporal dead zone

---
+++ 10 4Sep22/05 temporalDeadZone.js
@@ -0,0 +1,13 @@
+// Temporal Dead Zone - time taken b/w Scanning and getting the value of Let and const
+//  Till the time we are not fetch the value of Let and const that time is called as Temporal Dead Zone
+
+// example
+
+// console.log(b);
+// console.log(c);
+// const a = 10;
+
+// let b = 20;
+
+// console.log(b);
+// var c = 15;
