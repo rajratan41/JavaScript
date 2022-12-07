6d94a4bd3b11da83d1186dd9e2bd0f39fbb44f23
Raj Ratan <raj808341@gmail.com>
2022-12-07 21:03:06 +0530

    adding hw3

---
+++ homework/03 hw3.js
@@ -0,0 +1,4 @@
+// Question:- Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let coun = "Armenia";
+console.log(coun.toUpperCase());
