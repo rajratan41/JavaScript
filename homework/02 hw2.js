cee17350758d55432c635b7daa5c8563b88399af
Raj Ratan <raj808341@gmail.com>
2022-12-07 20:37:02 +0530

    adding Hw 2

---
+++ homework/02 hw2.js
@@ -0,0 +1,20 @@
+// Question:- Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = "Raj";
+let lastName = "Ratan";
+let maritalStatus = "Unmarried";
+let country = "India";
+let age = 22;
+
+// Displaying Using Interpolation Method
+
+console.log(
+    `
+    First Name is : ${firstName},
+    Last Name is : ${lastName},
+    Marital Status is : ${maritalStatus},
+    Age is : ${age},
+    Country is : ${country}
+
+    `
+);
