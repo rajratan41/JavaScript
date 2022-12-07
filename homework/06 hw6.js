671681ad45246cff9b5765b9d5d7cc816fcd4326
Raj Ratan <raj808341@gmail.com>
2022-12-07 21:17:03 +0530

    adding hw6

---
+++ homework/06 hw6.js
@@ -0,0 +1,5 @@
+// Question:- 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let arr = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+let result = arr.split();
+console.log(result);
