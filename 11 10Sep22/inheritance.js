81b92a715a41d7ab5b4e985c15474ffde7197816
Raj Ratan <raj808341@gmail.com>
2022-12-09 18:06:48 +0530

    updating inheritance

--- 11 10Sep22/inheritance.js
+++ 11 10Sep22/inheritance.js
@@ -1,1 +1,16 @@
 // Inheritance - When any class or any object is able to access  other class, object, method is called Inheritance
+
+class UserOne {
+    constructor (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName () {
+        const fullname = this.firstName + " " + this.lastName;
+        return fullname;
+    }
+}
+
+class UserTwo extends UserOne
+        
