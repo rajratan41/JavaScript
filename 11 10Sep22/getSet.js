4189bbe37b7a98056610eb00598dd828753cd3ff
Raj Ratan <raj808341@gmail.com>
2022-12-09 17:50:59 +0530

    updating getSet

--- 11 10Sep22/getSet.js
+++ 11 10Sep22/getSet.js
@@ -4,16 +4,48 @@
 
 // Example
 
+// class User {
+//     constructor (firstName, lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+
+//     get getFirstName () {
+//         return this.firstName;
+//     }
+//     get getLastName () {
+//         return this.lastName;
+//     }
+// }
+
+// let user1 = new User ("Raj", "Ratan");
+
+// console.log(user1.getFirstName);
+// console.log(user1.getLastName);
+
+
+// Setter = To set the Value
+
+// Example
+
 class User {
     constructor (firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.score = 0;
     }
 
-    get getFirstName () {
-        return this.firstName;
+    // Set
+    set setScore (score) {
+         this.score = score;
     }
-    get getLastName () {
-        return this.lastName;
+    // Get
+    get getScore () {
+        return this.score
     }
 }
+
+let user1 = new User ("Raj", "Ratan");
+
+user1.score = 8;
+console.log(user1.getScore);
