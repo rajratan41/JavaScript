cafb20a4a48e5b2e69345626f367e1b7fe305892
Raj Ratan <raj808341@gmail.com>
2022-12-09 18:24:48 +0530

    updating prototype

--- 11 10Sep22/prototype.js
+++ 11 10Sep22/prototype.js
@@ -1,19 +1,43 @@
-let UserName = function (name, count) {
-    this.name = name;
-    this.count = count;
-    this.getCourseCount = function () {
-        console.log (`The Course Count is ${this.count}`)
-    };
-};
+// let UserName = function (name, count) {
+//     this.name = name;
+//     this.count = count;
+//     this.getCourseCount = function () {
+//         console.log (`The Course Count is ${this.count}`)
+//     };
+// };
 
-let userOne = new UserName ("Raj Ratan", 8);
-userOne.getCourseCount();
+// let userOne = new UserName ("Raj Ratan", 8);
+// userOne.getCourseCount();
 
 
 // Adding New Method => getName inside UserName -
 
-UserName.prototype.getName = function () {
-    console.log(`The User Name is : ${this.name}`);
-}
+// UserName.prototype.getName = function () {
+//     console.log(`The User Name is : ${this.name}`);
+// }
 
-userOne.getName();
+// userOne.getName();
+
+
+
+// Prototyple Inheritance
+
+let objOne = {
+    fullName: "Raj Ratan",
+    address: "Bihar",
+    getName: function () {
+        console.log(this.fullName + "...." + this.address);
+    },
+};
+
+let objTwo = {
+    fullName: "Aman",
+};
+
+
+// I want to get- getName method from objOne to objTwo =>
+
+objTwo.__proto__ = objOne
+
+console.log(objTwo);
+objTwo.getName();
