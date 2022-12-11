26d73cd3a25068a7f24411250828e7d1a65e3412
Raj Ratan <raj808341@gmail.com>
2022-12-11 20:10:17 +0530

    adding async and await

--- 12 11Sep22/promise.js
+++ 12 11Sep22/promise.js
@@ -5,19 +5,58 @@
 
 
 
-const MakePromise = new Promise ((resolve, reject) => {
-    setTimeout (() => {
-        const arrOne = ["userOne", "userTwo", "userThree"];
-        if (arrOne.length > 0){
-            resolve ("User Found");
-        }else {
-            reject ("User not Found");
-        }
-    },2500);
-});
+// const MakePromise = new Promise ((resolve, reject) => {
+//     setTimeout (() => {
+//         const arrOne = ["userOne", "userTwo", "userThree"];
+//         if (arrOne.length > 0){
+//             resolve ("User Found");
+//         }else {
+//             reject ("User not Found");
+//         }
+//     },2500);
+// });
 
-MakePromise.then((result) => {
-    console.log(result);
-}) .catch ((result) => {
-    console.log(result);
-});
+// Syntax - MakePromise.then().catch()
+
+// console.log(MakePromise.then());
+
+
+// MakePromise.then((result) => {
+//     console.log(result);
+// }) .catch ((result) => {
+//     console.log(result);
+// });
+
+
+
+
+// async and await
+
+const userOne = () => {
+    return "I am User One";
+};
+
+const userTwo = () => {
+    return new Promise ((resolve, reject) => {
+        setTimeout (() => {
+            resolve ("User Two Found");
+        }, 2000)
+    });
+};
+
+const userThree = () => {
+    return "I am User Three";
+};
+
+const waiter = async () => {
+    let guestOne = userOne();
+    console.log(guestOne);
+
+    let guestTwo = await userTwo();
+    console.log(guestTwo);
+
+    let guestThree = userThree();
+    console.log(guestThree);
+};
+
+waiter();
