9fa1537c1b54d0689a10d8890aa5d7ae5feb70ea
Raj Ratan <raj808341@gmail.com>
2022-12-14 21:24:06 +0530

    adding Project-1 ColorChanger

---
+++ CangeColor-Project1/script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("button");
+
+// Give HEX value
+const randomColor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+
+    for (let i=0; i<6; i++) {
+        cons = cons + val [Math.floor(Math.random() * 16)];
+    }
+    return cons;
+};
+
+function changeColor () {
+    document.body.style.backgroundColor = randomColor();
+}
+
+btn.addEventListener("click", changeColor);
